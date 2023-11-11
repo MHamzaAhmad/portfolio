3ad5c9c82710e472349d6c9c9b1baa9394ebe481
Hamza <hmzaawan88@gmail.com>
2023-11-11 22:24:59 +0500

    Update name

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -6,7 +6,7 @@ const MainHeader = () => {
       <nav className="grid-cols-3 border-y border-y-border-color h-[3.51195rem] laptop:grid hidden">
         <div className="col-span-2 flex">
           <div className="text-center pl-[1.38rem] pr-[9.25rem] border-r border-r-border-color flex items-center">
-            hamza-ahmad
+            muhammad-hamza
           </div>
           <MenuItem name="_hello" route="/" />
           <MenuItem name="_about-me" route="/about-me" />
