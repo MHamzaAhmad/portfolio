c454edd0a7c5bfd396e59935a13e4bdce39d0832
Hamza <hmzaawan88@gmail.com>
2023-11-12 15:03:53 +0500

    Remove event

--- src/config/Providers.tsx
+++ src/config/Providers.tsx
@@ -2,19 +2,8 @@
 import { CacheProvider } from "@chakra-ui/next-js";
 import { ChakraProvider } from "@chakra-ui/react";
 import theme from "./theme";
-import { useEffect } from "react";
-import { isSupported, logEvent } from "firebase/analytics";
-import { analytics } from "./firebase";
 
 function Providers({ children }: { children: React.ReactNode }) {
-  useEffect(() => {
-    isSupported().then((isSupported) => {
-      if (isSupported) {
-        logEvent(analytics, "app_viewed");
-      }
-    });
-  }, []);
-
   return (
     <CacheProvider>
       <ChakraProvider theme={theme}>{children}</ChakraProvider>
