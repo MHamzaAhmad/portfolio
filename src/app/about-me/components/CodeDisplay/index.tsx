e90c89e7156b6e4a730558edf1d2cc83b5effa80
Hamza <hmzaawan88@gmail.com>
2024-05-26 11:30:13 -0400

    Updated the about me page

--- src/app/about-me/components/CodeDisplay/index.tsx
+++ src/app/about-me/components/CodeDisplay/index.tsx
@@ -1,5 +1,6 @@
 "use client";
 import Heading from "@/components/Base/Heading";
+import { Card, CardBody, CardHeader } from "@chakra-ui/react";
 import MarkdownPreview from "@uiw/react-markdown-preview";
 import { FC } from "react";
 
@@ -11,10 +12,22 @@ interface Props {
 const CodeDisplay: FC<Props> = ({ source, title }) => {
   return (
     <>
-      <Heading>{`> ${title}`}</Heading>
-      <div className="bg-code-back my-4 rounded-md">
-        <MarkdownPreview disableCopy source={source} />
-      </div>
+      <Card
+        className="mb-4 hover:drop-shadow-xl transition duration-300 ease-in-out"
+        background="var(--background-color)"
+        variant="outline"
+        borderColor="var(--border-color)"
+        borderRadius="0.9375rem"
+      >
+        <CardHeader>
+          <Heading>{`> ${title}`}</Heading>
+        </CardHeader>
+        <CardBody>
+          <div className="bg-code-back my-4 rounded-md">
+            <MarkdownPreview disableCopy source={source} />
+          </div>
+        </CardBody>
+      </Card>
     </>
   );
 };
