24cfbe7f80b3d2f66434865e4459f40a09ec2846
Hamza <hmzaawan88@gmail.com>
2025-05-22 18:09:57 +0500

    update resume

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -1,2 +1,2 @@
 export const RESUME_PFD =
-  "https://firebasestorage.googleapis.com/v0/b/portfolio-dae28.appspot.com/o/hamza_resume_july.pdf?alt=media&token=7cfbbf3c-af4c-4b1a-8a11-03b6ef87a386";
+  "https://firebasestorage.googleapis.com/v0/b/portfolio-dae28.appspot.com/o/hamza_resume_july.pdf?alt=media&token=8d42ead7-a70f-4dc1-99f8-70d53923cfbf";
